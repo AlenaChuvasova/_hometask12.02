760ddb43a76c1c6adb8218e4276cd59138497c18
Alena <39169371+AlenaChuvasova@users.noreply.github.com>
2019-07-20 13:14:04 +0300

    Update Main.java

--- src/Task1/Main.java
+++ src/Task1/Main.java
@@ -4,12 +4,9 @@ public class Main {
     public static int square (int n){
         int res = n*n;
         return res;
-
     }
-
 
     public static void main(String[] args) {
         System.out.println(square(4));
-
     }
 }
